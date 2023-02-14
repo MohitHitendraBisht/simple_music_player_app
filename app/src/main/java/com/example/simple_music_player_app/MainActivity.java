284e910a67c5eca43c1935c8674631218ba9f8b3
Mohit Hitendra Bisht <82029867+MohitHitendraBisht@users.noreply.github.com>
2023-02-15 01:46:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/simple_music_player_app/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.simple_music_player_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.media.MediaPlayer;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity
+        extends AppCompatActivity {
+
+    // Instantiating the MediaPlayer class
+    MediaPlayer music;
+
+    @Override
+    protected void onCreate(
+            Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Adding the music file to our
+        // newly created object music
+        music = MediaPlayer.create(
+                this, R.raw.sound);
+    }
+
+    // Playing the music
+    public void musicplay(View v)
+    {
+        music.start();
+    }
+
+    // Pausing the music
+    public void musicpause(View v)
+    {
+        music.pause();
+    }
+
+    // Stopping the music
+    public void musicstop(View v)
+    {
+        music.stop();
+        music
+                = MediaPlayer.create(
+                this, R.raw.sound);
+    }
+}
